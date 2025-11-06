db17cb1bcac9f30ae514501dba19201e68e9a8f3
Caleb Myers <cmyers5108@gmail.com>
2025-11-06 09:54:42 -0500

    fix error

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 import { API_BASE_URL } from "../constants/api";
-import { makeAuthenticatedRequest } from "../composables/useApi";
 import type { SpotifyUser } from "../types/spotify";
 
 interface UserState {
